c1d5144d6ebad6bf145b8527b6a04ecca3b41801
Tim <timpei123@gmail.com>
2015-06-09 22:52:34 +0800

    parsing module complete

--- silent_selenium.py
+++ silent_selenium.py
@@ -2,13 +2,19 @@
 
 from pyvirtualdisplay import Display
 from selenium import webdriver
+from BeautifulSoup import BeautifulSoup
 
 display = Display(visible = 0, size = (800, 600))
 display.start()
 
 browser = webdriver.Firefox()
-browser.get('http://www.google.com')
-print browser.title()
+browser.get('http://new.comicvip.com/show/cool-103.html?ch=586-2')
+
+print browser.title
+#print browser.page_source
+
+soup = BeautifulSoup(browser.page_source)
+print (soup.find(id='TheImg').get('src'))#expect to get the comic pic...
 
 browser.quit()
 
