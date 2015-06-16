e41632e005bd98f96097fd993411338e7ae1ed84
Tim <timpei123@gmail.com>
2015-06-17 03:02:51 +0800

    search automation complete

--- search_automaiton.py
+++ search_automaiton.py
@@ -5,25 +5,53 @@ from pyvirtualdisplay import Display
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import sys
+import codecs
 
 search = sys.argv[1].decode("utf-8")
 
-display = Display(visible = 0, size = (800,  600))
-display.start()
+#display = Display(visible = 0, size = (800,  600))
+#display.start()
 
 browser = webdriver.Firefox()
 browser.get('http://comicvip.com/')#open the homepage
-#browser.type("id=k", search)#type in the serach
+#type in the serach
 box = browser.find_element_by_id('k')
 box.send_keys(search)
 button = browser.find_element_by_id('bt')
 webdriver.ActionChains(browser).move_to_element(button).click(button).perform()
-#browser.click("//td[2]/a")#click in the first result
+#click in the first result
+
+print browser.current_url
+
 res = browser.find_element_by_xpath("//td[2]/a")
 webdriver.ActionChains(browser).move_to_element(res).click(res).perform()
 
-print browser.title
+print browser.current_url
+
+base = browser.find_element_by_id("c1")
+base.click()
+print base.text
+for handle in browser.window_handles:
+	browser.switch_to_window(handle)
+
+print browser.current_url
+'''
+vol_list = browser.find_elements_by_class_name('Vol')
+epi_list = browser.find_elements_by_class_name('Ch')
+
+
+
+with codecs.open('url_list.txt', 'w', "utf-8") as f:
+	
+	#print 'Vols:'
+	for v in vol_list:
+		f.write("%s	%s\n" % (v.text, v.get_attribute("id")))
+
+	#print 'Episodes:'
+	for e in epi_list:
+		f.write("%s	%s\n" % (e.text, e.get_attribute("id")))
+'''
 
 browser.quit()
 
-display.stop()
+#display.stop()
