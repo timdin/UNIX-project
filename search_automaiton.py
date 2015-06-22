59bdc2ab961b51bb82ed796c0e94afd41e2f16dd
Tim <timpei123@gmail.com>
2015-06-22 23:41:47 +0800

    modified search_automation.py

--- search_automaiton.py
+++ search_automaiton.py
@@ -13,63 +13,58 @@ search = sys.argv[1].decode("utf-8")
 display = Display(visible = 0, size = (800,  600))
 display.start()
 
+print "searching..."
+
+#open the homepage
 browser = webdriver.Firefox()
-browser.get('http://comicvip.com/')#open the homepage
+browser.get('http://comicvip.com/')
+
 #type in the serach
 box = browser.find_element_by_id('k')
 box.send_keys(search)
+
+#click in the first result
 button = browser.find_element_by_id('bt')
 webdriver.ActionChains(browser).move_to_element(button).click(button).perform()
-#click in the first result
 
-print browser.current_url
+print "search done!"
 
 res = browser.find_element_by_xpath("//td[2]/a")
 webdriver.ActionChains(browser).move_to_element(res).click(res).perform()
 
-print browser.current_url
+print "entering comic index"
 
-browser.execute_script("cview('102-51.html',6);")
-
-
-for handle in browser.window_handles:
-	browser.switch_to_window(handle)
-
-print browser.current_url
-
-
-'''
-base = browser.find_element_by_id("c1")
-
-webdriver.ActionChains(browser) \
-	.move_to_element(base) \
-	.key_down(Keys.CONTROL) \
-	.click(base) \
-	.key_up(Keys.CONTROL).perform()
-
-print base.text
-for handle in browser.window_handles:
-	browser.switch_to_window(handle)
-
-print browser.current_url
-'''
-'''
 vol_list = browser.find_elements_by_class_name('Vol')
+vol_data = {}
+for v in vol_list:
+	vol_data[v.get_attribute("id")] = v.text
+	#print vol_data[v.get_attribute("id")]
+
 epi_list = browser.find_elements_by_class_name('Ch')
+epi_data = {}
+for e in epi_list:
+	epi_data[e.get_attribute("id")] = e.text
+	#print epi_data[e.get_attribute("id")]
+
+script = browser.find_element_by_id("c1").get_attribute('onclick')
+
+browser.execute_script(script)
 
 
+for handle in browser.window_handles:
+	browser.switch_to_window(handle)
+
+print browser.current_url
 
 with codecs.open('url_list.txt', 'w', "utf-8") as f:
-	
+
+	f.write("%s\n" % browser.current_url[:-1])
 	#print 'Vols:'
-	for v in vol_list:
-		f.write("%s	%s\n" % (v.text, v.get_attribute("id")))
-
+	for vid, vtext in vol_data.iteritems():
+		f.write("%s\t%s\n"%(vid, vtext))
 	#print 'Episodes:'
-	for e in epi_list:
-		f.write("%s	%s\n" % (e.text, e.get_attribute("id")))
-'''
-
+	for eid, etext in epi_data.iteritems():
+		f.write("%s\t%s\n"%(eid, etext))
 browser.quit()
 
 display.stop()
