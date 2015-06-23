489d7932a26abe7b5b4c2e0548e4acb46891b5d4
Tim <timpei123@gmail.com>
2015-06-23 11:48:04 +0800

    page_automation done

--- page_automation.py
+++ page_automation.py
@@ -4,6 +4,7 @@ from pyvirtualdisplay import Display
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import codecs
+import time
 
 vol_list = []
 
@@ -20,16 +21,19 @@ display = Display(visible = 0, size = (800, 600))
 display.start()
 
 browser = webdriver.Firefox()
+browser.implicitly_wait(10)
+
 for vol in vol_list:
 	browser.get(base_url+vol)
+	soup = BeautifulSoup(browser.page_source)
 	#get all pages
 	for option in soup.find_all('option'):
-		browser.get(base_url+vol+"="+option['value'])
-		soup = BeautifulSoup(browser.page_source)
-		print (soup.find(id='TheImg').get('src'))#expect to get the comic pic...
+		print option['value']
+		browser.get(base_url+vol+"-"+option['value'])
+		print browser.current_url
+		pagesoup = BeautifulSoup(browser.page_source)
+		print (pagesoup.find(id='TheImg').get('src'))#expect to get the comic pic...
+		time.sleep(5)
 
-	
-	
 browser.quit()
-
 display.stop()
